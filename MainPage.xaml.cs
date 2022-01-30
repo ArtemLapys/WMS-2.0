c273b1edf3edf879ac0b717bcdc01b636cfd3c6f
А.Лапис <77628969+ArtemLapys@users.noreply.github.com>
2022-01-30 14:45:14 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+
+using System.Windows;
+using Windows.Media.Control;
+
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace WMS_2._0
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(directoryPage));
+
+           
+        }
+    }
+
+
+}
---
+++ directoryPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace WMS_2._0
+{
+
+    public sealed partial class directoryPage : Page
+    {
+
+        public directoryPage()
+        {
+            this.InitializeComponent();
+           //directory("n");
+
+        }
+
+        //Methods
+        public void directory(string path)
+        {
+            Directory.Text = path;
+        }
+
+
+        public static bool TryGoBack()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+                return true;
+            }
+            return false;
+        }
+
+
+        // Buttons Click //
+
+        //Button Back
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            TryGoBack();
+        }
+        
+        //Button New Directory
+        async private void NewDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var savePath = new Windows.Storage.Pickers.FileSavePicker();
+            savePath.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            savePath.FileTypeChoices.Add("Text document", new List<string>() { ".txt" });
+            savePath.SuggestedFileName = "Track Info(WMS)";
+            Windows.Storage.StorageFile file = await savePath.PickSaveFileAsync();
+            if (file != null)
+            {
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("PickedFolderToken", file);
+                string pathFile = file.Path;
+                directory(pathFile);
+            }
+            else
+            {
+                directory("We could not find the path to the saved file. There may have been some mistake😭");
+            }
+
+
+        }
+    }
+}
